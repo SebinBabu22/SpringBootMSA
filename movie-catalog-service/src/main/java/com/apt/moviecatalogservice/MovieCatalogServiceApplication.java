cbc66a3a68b17f1f32a82467a84970261cc30416
User <User@SYSTEM1>
2019-11-27 11:13:08 +0530

    1st commit

---
+++ movie-catalog-service/src/main/java/com/apt/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -0,0 +1,14 @@
+package com.apt.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		//SpringBoot
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+	}
+
+}
