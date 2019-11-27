3faffd6169e10a523682f28f84643a8d7120dd71
User <User@SYSTEM1>
2019-11-27 15:38:31 +0530

    After bean implementation

--- movie-catalog-service/src/main/java/com/apt/moviecatalogservice/resourse/MovieCatalogResourse.java
+++ movie-catalog-service/src/main/java/com/apt/moviecatalogservice/resourse/MovieCatalogResourse.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.apt.moviecatalogservice.models.CatalogItem;
 import com.apt.moviecatalogservice.models.Movie;
@@ -18,32 +20,29 @@ import com.apt.moviecatalogservice.models.Rating;
 @RequestMapping("/catalog")
 public class MovieCatalogResourse {
 	
+	    @Autowired
+	    private RestTemplate restTemplate;
+
+	    @Autowired
+	    WebClient.Builder webClientBuilder;
+
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
 	{
 		
-		RestTemplate restTemplate =new RestTemplate();
-		//Movie movie = restTmp.getForObject("http://localhost:8081/movies/foo", Movie.class);
-		//get all rated movie IDs
-		/*List<Rating> ratingsList = Arrays.asList(
-                new Rating("1234", 3),
-                new Rating("5678", 4)
-        );*/
-		//List<String> ratings = Rating.stream().map(x -> x.getName()).collect(Collectors.toList());
-				
-	/*	return ratings.stream()
-                .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + ratings.ge, Movie.class);
-                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
-                })
-                .collect(Collectors.toList());*/
-	    		
-				
 		
-		// For each movie id  call movie info service and get details
 		
-		// Put all together
 		
+		// UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+/*
+	        return userRating.getRatings().stream()
+	                .map(rating -> {
+	                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+	                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+	                })
+	                .collect(Collectors.toList());*/
+
 		return Collections.singletonList( new CatalogItem("BigB","Super", 4));
              
              
